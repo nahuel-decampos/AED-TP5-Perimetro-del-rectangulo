571986d2a0d328463c0d539ab479274f592295ee
nahuel-decampos <nahuel.decampos@gmail.com>
2015-05-16 10:34:36 -0300

    Create TP5.cpp
    Tiene errores

---
+++ TP5.cpp
@@ -0,0 +1,31 @@
+/* Perimetro del rectangulo
+   Nahuel de Campos
+   12/05/15
+*/
+
+#include <iostream>
+using namespace std;
+
+double ObtNumPosit ();
+double CalcPerim (double, double);
+
+int main (){
+    cout << "\n   Programa para calcular el perimetro de un rectangulo  ";
+    cout << "\n   ----------------------------------------------------";
+    cout << "\n Ingrese el valor de la base (en cms): ";
+    double b = ObtNumPosit();
+    cout << "\n Ingrese el valor de la altura (en cms): ";
+    double h = ObtNumPosit();
+    cout << "\n El perimetro del rectangulo es de " << CalcPerim (h,b) << " cms.\n";
+}
+
+double ObtNumPosit (){
+    double num =0;
+    while (num <= 0)
+        cin >> num;
+}
+
+double CalcPerim (double base, double altura){
+    return 2.0*(base + altura);
+}
+
